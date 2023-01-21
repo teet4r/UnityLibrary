13925e811b1b612e81aa5eca8b635102747bc2e8
teet4r <76508241+teet4r@users.noreply.github.com>
2023-01-21 15:31:37 +0900

    AutoReturnToPool 추가
    자동으로 풀에 반환

---
+++ AutoReturnToPool.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoReturnToPool : MonoBehaviour
+{
+    [Min(0f)][SerializeField] float _returnTime;
+
+    WaitForSecondsRealtime _wfsrt = null;
+
+    void Awake()
+    {
+        _wfsrt = new WaitForSecondsRealtime(_returnTime);
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(_ReturnToPool());
+    }
+
+    IEnumerator _ReturnToPool()
+    {
+        yield return _wfsrt;
+
+        PoolManager.instance.Put(gameObject);
+    }
+}
