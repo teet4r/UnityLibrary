0543f2c90597278fa9e9ca759de91e63f2868894
teet4r <76508241+teet4r@users.noreply.github.com>
2023-02-03 15:23:14 +0900

    230203 Update
    SoundManager 개편

--- SoundManager.cs
+++ SoundManager.cs
@@ -1,105 +1,25 @@
 ﻿using UnityEngine;
 
-public class SoundManager : Singleton<SoundManager>
+public class SoundManager : PrefabricatedSingleton<SoundManager>
 {
-    public BgmAudio BgmAudio { get; private set; } = null;
-    public SfxAudio SfxAudio { get; private set; } = null;
+    public BgmAudio BgmAudio { get { return _bgmAudio; } }
+    public SfxAudio SfxAudio { get { return _sfxAudio; } }
 
-    bool _isCreated = false;
-
-    protected override void Awake()
-    {
-        base.Awake();
-
-        Create();
-    }
-
-    public void Create()
-    {
-        if (_isCreated) return;
-        _isCreated = true;
-
-        // BgmAudio 생성
-        var newObj0 = new GameObject("BgmAudio");
-        newObj0.transform.parent = transform;
-        BgmAudio = newObj0.AddComponent<BgmAudio>();
-        
-        // SfxAudio 생성
-        var newObj1 = new GameObject("SfxAudio");
-        newObj1.transform.parent = transform;
-        SfxAudio = newObj1.AddComponent<SfxAudio>();
-    }
+    [SerializeField] BgmAudio _bgmAudio = null;
+    [SerializeField] SfxAudio _sfxAudio = null;
 }
 
-public abstract class BaseAudio : MonoBehaviour
+public enum Bgm
 {
-    public bool IsMute
-    {
-        get { return _audioSource.mute; }
-        set { _audioSource.mute = value; }
-    }
-    public float Volume
-    {
-        get { return _audioSource.volume; }
-        set { _audioSource.volume = value; }
-    }
 
-    [SerializeField] protected AudioSource _audioSource = null;
-
-    protected virtual void Awake()
-    {
-        if (_audioSource == null)
-        {
-            if (TryGetComponent(out AudioSource audioSource))
-                _audioSource = audioSource;
-            else
-                _audioSource = gameObject.AddComponent<AudioSource>();
-        }
-    }
-
-    public abstract void Play(string name);
 }
 
-public class BgmAudio : BaseAudio
+public enum Sfx
 {
-    protected override void Awake()
-    {
-        base.Awake();
-
-        _audioSource.playOnAwake = false;
-        _audioSource.loop = true;
-    }
-
-    public override void Play(string name)
-    {
-        var clip = ResourceManager.Instance.Get<AudioClip>(name);
-        if (clip == null)
-        {
-            Debug.LogError($"{name} bgm is not found.");
-            return;
-        }
-        _audioSource.clip = clip;
-        _audioSource.Play();
-    }
-}
-
-public class SfxAudio : BaseAudio
-{
-    protected override void Awake()
-    {
-        base.Awake();
-
-        _audioSource.playOnAwake = false;
-    }
-
-    public override void Play(string name)
-    {
-        var clip = ResourceManager.Instance.Get<AudioClip>(name);
-        if (clip == null)
-        {
-            Debug.LogError($"{name} sfx is not found.");
-            return;
-        }
-        _audioSource.PlayOneShot(clip);
-    }
+    EnemyKilled0, EnemyKilled1, EnemyKilled2,
+    BulletHit,
+    Flash,
+    Gun,
+    LightOff,
+    LightOn,
 }
